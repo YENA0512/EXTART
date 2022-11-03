c17473f9c799b8435c251eadffed3b24450dbe97
YENA0512 <53147797+YENA0512@users.noreply.github.com>
2022-11-03 14:00:53 +0900

    [fix] change whole layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-const list = document.querySelector('.slide__list');
-const items = document.querySelectorAll('.slide__item');
-const buttons = document.querySelector('.buttons');
+const list = document.querySelector(".slide__list");
+const items = document.querySelectorAll(".slide__item");
+const buttons = document.querySelector(".buttons");
 
 const lastIndex = items.length - 1;
 let selected = 0;
@@ -13,96 +13,93 @@ const setTransition = (value) => {
 
 const setTranslate = ({ index, reset }) => {
   if (reset) list.style.transform = `translate(-${list.clientWidth}px, 0)`;
-  else list.style.transform = `translate(-${(index + 1) * list.clientWidth}px, 0)`;
+  else
+    list.style.transform = `translate(-${(index + 1) * list.clientWidth}px, 0)`;
 };
 
 const activePagination = (index) => {
   [...paginations.children].forEach((pagination) => {
-    pagination.classList.remove('on');
+    pagination.classList.remove("on");
   });
-  paginations.children[index].classList.add('on');
+  paginations.children[index].classList.add("on");
 };
-
-
 
 // Make prev and next buttons.
 const handlePrev = () => {
-    selected -= 1;
-    setTransition('transform 1s linear');
-    setTranslate({ index: selected });
-    if (selected < 0) {
-      selected = lastIndex;
-      setTimeout(() => {
-        setTransition('');
-        setTranslate({ index: selected });
-      }, 1000);
-    }
-    if (selected >= 0) activePagination(selected);
-  };
-  
-  const handleNext = () => {
-    console.log(selected);
-    selected += 1;
-    setTransition('transform 1s linear');
-    setTranslate({ index: selected });
-    if (selected > lastIndex) {
-      selected = 0;
-      setTimeout(() => {
-        setTransition('');
-        setTranslate({ index: selected });
-      }, 1000);
-    }
+  selected -= 1;
+  setTransition("transform 1s linear");
+  setTranslate({ index: selected });
+  if (selected < 0) {
+    selected = lastIndex;
+    setTimeout(() => {
+      setTransition("");
+      setTranslate({ index: selected });
+    }, 1000);
+  }
+  if (selected >= 0) activePagination(selected);
+};
 
-  };
-  
-  const makeButton = () => {
-    if (items.length > 1) {
-      const prevButton = document.createElement('button');
-      prevButton.classList.add('buttons__prev');
-      prevButton.innerHTML = '<img src="./assets/img/arrowL.png">';
-      prevButton.addEventListener('click', handlePrev);
-  
-      const nextButton = document.createElement('button');
-      nextButton.classList.add('buttons__next');
-      nextButton.innerHTML = '<img src="./assets/img/arrowR.png">';
-      nextButton.addEventListener('click', handleNext);
-  
-      buttons.appendChild(prevButton);
-      buttons.appendChild(nextButton);
-    }
-  };
+const handleNext = () => {
+  console.log(selected);
+  selected += 1;
+  setTransition("transform 1s linear");
+  setTranslate({ index: selected });
+  if (selected > lastIndex) {
+    selected = 0;
+    setTimeout(() => {
+      setTransition("");
+      setTranslate({ index: selected });
+    }, 1000);
+  }
+};
 
-  // Clone the first and last elements.
+const makeButton = () => {
+  if (items.length > 1) {
+    const prevButton = document.createElement("button");
+    prevButton.classList.add("buttons__prev");
+    prevButton.innerHTML = '<img src="./assets/img/arrowL.png">';
+    prevButton.addEventListener("click", handlePrev);
+
+    const nextButton = document.createElement("button");
+    nextButton.classList.add("buttons__next");
+    nextButton.innerHTML = '<img src="./assets/img/arrowR.png">';
+    nextButton.addEventListener("click", handleNext);
+
+    buttons.appendChild(prevButton);
+    buttons.appendChild(nextButton);
+  }
+};
+
+// Clone the first and last elements.
 const cloneElement = () => {
-    list.prepend(items[lastIndex].cloneNode(true));
-    list.append(items[0].cloneNode(true));
-    setTranslate({ reset: true });
-  };
+  list.prepend(items[lastIndex].cloneNode(true));
+  list.append(items[0].cloneNode(true));
+  setTranslate({ reset: true });
+};
 
-
-  // Automatically play the slide
+// Automatically play the slide
 const autoplayIterator = () => {
-    selected += 1;
-    setTransition('all 1s linear');
-    setTranslate({ index: selected });
-    if (selected > lastIndex) {
-      clearInterval(interval);
-      setTimeout(() => {
-        selected = 0;
-        setTransition('');
-        setTranslate({ reset: true });
-        autoplay({ duration: 8000 });
-      }, 1000);
-    }
-  };
-  
-  const autoplay = ({ duration }) => {
-    interval = setInterval(autoplayIterator, duration);
-  };
+  selected += 1;
+  setTransition("all 1s linear");
+  setTranslate({ index: selected });
+  if (selected > lastIndex) {
+    clearInterval(interval);
+    setTimeout(() => {
+      selected = 0;
+      setTransition("");
+      setTranslate({ reset: true });
+      autoplay({ duration: 8000 });
+    }, 1000);
+  }
+};
 
-  const render = () => {
-    // makeButton();
-    cloneElement();
-    autoplay({ duration: 8000 });
-  };
-  render();
+const autoplay = ({ duration }) => {
+  interval = setInterval(autoplayIterator, duration);
+};
+
+const render = () => {
+  // makeButton();
+  cloneElement();
+  autoplay({ duration: 8000 });
+};
+render();
--- start/js/start.js
+++ start/js/start.js
@@ -4,33 +4,33 @@ const endPoint = 3;
 const select = [];
 const result = document.querySelector("#result");
 
-function calResult(){
+function calResult() {
   var pointArray = [
-    { name: '수영', value: 0, key: 0 },
-    { name: '테니스', value: 0, key: 1 },
-    { name: '골프', value: 0, key: 2 },
-    { name: '복싱', value: 0, key: 3 },
-    { name: '축구', value: 0, key: 4 },
-    { name: '클라이밍', value: 0, key: 5 },
-    { name: '필라테스', value: 0, key: 6 },
-    { name: '크로스핏', value: 0, key: 7 },
-    { name: '요가', value: 0, key: 8 },
-  ]
-  for(let i = 0; i < endPoint; i++){
+    { name: "수영", value: 0, key: 0 },
+    { name: "테니스", value: 0, key: 1 },
+    { name: "골프", value: 0, key: 2 },
+    { name: "복싱", value: 0, key: 3 },
+    { name: "축구", value: 0, key: 4 },
+    { name: "클라이밍", value: 0, key: 5 },
+    { name: "필라테스", value: 0, key: 6 },
+    { name: "크로스핏", value: 0, key: 7 },
+    { name: "요가", value: 0, key: 8 },
+  ];
+  for (let i = 0; i < endPoint; i++) {
     var target = qnaList[i].a[select[i]];
-    for(let j = 0; j< target.type.length; j++){
-      for(let k = 0;k<pointArray.length;k++){
-        if(target.type[j] === pointArray[k].name){
+    for (let j = 0; j < target.type.length; j++) {
+      for (let k = 0; k < pointArray.length; k++) {
+        if (target.type[j] === pointArray[k].name) {
           pointArray[k].value += 1;
         }
       }
     }
   }
-  var resultArray = pointArray.sort(function (a,b){
-    if (a.value > b.value){
+  var resultArray = pointArray.sort(function (a, b) {
+    if (a.value > b.value) {
       return -1;
     }
-    if (a.value < b.value){
+    if (a.value < b.value) {
       return 1;
     }
     return 0;
@@ -40,44 +40,47 @@ function calResult(){
 
   return resultword;
 }
-function setResult(){
+function setResult() {
   let point = calResult();
   console.log(point);
-  const resultName = document.querySelector('.resultname');
-  resultName.innerHTML = `<h1>당신에게 맞는 운동은 &nbsp&nbsp&nbsp<strong><span class="s1">` +  infoList[point].name + `</span></strong> &nbsp&nbsp&nbsp   입니다</h1><br>`;
-  var resultImg = document.createElement('img');
-  const resultHashtagNum = 'hashtag' + (point+1);
+  const resultName = document.querySelector(".resultname");
+  resultName.innerHTML =
+    `<h1>당신에게 맞는 운동은 &nbsp&nbsp&nbsp<strong><span class="s1">` +
+    infoList[point].name +
+    `</span></strong> &nbsp&nbsp&nbsp   입니다</h1><br>`;
+  var resultImg = document.createElement("img");
+  const resultHashtagNum = "hashtag" + (point + 1);
   console.log("'" + resultHashtagNum + "'");
-  const imgDiv = document.querySelector('#resultImg');
-  var imgURL = './img/image-' + point + '.jpg';
+  const imgDiv = document.querySelector("#resultImg");
+  var imgURL = "./img/image-" + point + ".jpg";
   resultImg.src = imgURL;
   resultImg.alt = point;
-  resultImg.classList.add('img-fluid2');
+  resultImg.classList.add("img-fluid3");
   imgDiv.appendChild(resultImg);
-  const resultDesc = document.querySelector('.resultDesc');
+  const resultDesc = document.querySelector(".resultDesc");
   resultDesc.innerHTML = infoList[point].desc1;
-  var testbtn = document.createElement('button');
-  testbtn.type = 'button';
-  testbtn.innerHTML = '더 알아보기';
-  testbtn.className = 'testbtn';
-  testbtn.addEventListener('click',e=>{
-    window.location = '../detail.html';
+  var testbtn = document.createElement("button");
+  testbtn.type = "button";
+  testbtn.innerHTML = "더 알아보기";
+  testbtn.className = "testbtn";
+  testbtn.addEventListener("click", (e) => {
+    window.location = "../detail.html";
   });
   resultDesc.appendChild(testbtn);
-  var resetbtn = document.createElement('button');
-  resetbtn.type = 'button';  
-  resetbtn.src = '../assets/img/Frame.png';
-  resetbtn.className = 'resetbtn';
-  var resetImg = document.createElement('img');
-  resetImg.src = '../assets/img/Frame.png';
+  var resetbtn = document.createElement("button");
+  resetbtn.type = "button";
+  resetbtn.src = "../assets/img/Frame.png";
+  resetbtn.className = "resetbtn";
+  var resetImg = document.createElement("img");
+  resetImg.src = "../assets/img/Frame.png";
   resetbtn.appendChild(resetImg);
-  resetbtn.addEventListener('click',e=>{
-    window.location = './test.html';
+  resetbtn.addEventListener("click", (e) => {
+    window.location = "./test.html";
   });
   resultDesc.appendChild(resetbtn);
-  return point+1;
+  return point + 1;
 }
-function goResult(){
+function goResult() {
   qna.style.WebkitAnimation = "fadeOut 0.5s";
   qna.style.animation = "fadeOut 0.5s";
   setTimeout(() => {
@@ -86,51 +89,56 @@ function goResult(){
     setTimeout(() => {
       qna.style.display = "none";
       result.style.display = "block";
-    }, 0)})
+    }, 0);
+  });
 
-    console.log(select);
-    setResult();
-    calResult();
+  console.log(select);
+  setResult();
+  calResult();
 }
-function addAnswer(answerText,qIdx,idx){
-  var a = document.querySelector('.answerBox');
-  var answer = document.createElement('button');
-  answer.classList.add('answerList');
-  answer.style.marginBottom = '54px';
-   //answer.classList.add('style:"margin-bottom:54px";');
+function addAnswer(answerText, qIdx, idx) {
+  var a = document.querySelector(".answerBox");
+  var answer = document.createElement("button");
+  answer.classList.add("answerList");
+  answer.style.marginBottom = "54px";
+  //answer.classList.add('style:"margin-bottom:54px";');
   // answer.classList.add('py-4');
-  answer.classList.add('mx-auto');
-  answer.classList.add('fadeIn');
+  answer.classList.add("mx-auto");
+  answer.classList.add("fadeIn");
   a.appendChild(answer);
   answer.innerHTML = answerText;
-  answer.addEventListener("click",function(){
-    var children = document.querySelectorAll('.answerList');
-    for(let i = 0; i<children.length;i++){
-      children[i].disabled = true;
-      children[i].style.WebkitAnimation = "fadeOut 0.5s";
-      children[i].style.animation = "fadeOut 0.5s";
-    }
-    setTimeout(() => {
-      select[qIdx] = idx;
-      for(let i = 0; i<children.length;i++){
-        children[i].style.display = 'none';
+  answer.addEventListener(
+    "click",
+    function () {
+      var children = document.querySelectorAll(".answerList");
+      for (let i = 0; i < children.length; i++) {
+        children[i].disabled = true;
+        children[i].style.WebkitAnimation = "fadeOut 0.5s";
+        children[i].style.animation = "fadeOut 0.5s";
       }
+      setTimeout(() => {
+        select[qIdx] = idx;
+        for (let i = 0; i < children.length; i++) {
+          children[i].style.display = "none";
+        }
 
-    goNext(++qIdx);
-  },450)
-  },false);
+        goNext(++qIdx);
+      }, 450);
+    },
+    false
+  );
 }
-function goNext(qIdx){
-  if(qIdx === endPoint) {
+function goNext(qIdx) {
+  if (qIdx === endPoint) {
     goResult();
     return;
   }
-  var q = document.querySelector('.qBox');
-  q.style.marginLeft = 'auto';
-  q.style.marginRight = 'auto';
+  var q = document.querySelector(".qBox");
+  q.style.marginLeft = "auto";
+  q.style.marginRight = "auto";
   q.innerHTML = qnaList[qIdx].q;
-  for(let i in qnaList[qIdx].a){
-    addAnswer(qnaList[qIdx].a[i].answer,qIdx,i);
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
   // var status = document.querySelector('.statusBar');
   // status.style.width=(100/endPoint) * (qIdx+1) + '%';
@@ -142,8 +150,7 @@ function goNext(qIdx){
 //   console.log(location.href);
 // };
 
-
-function begin(){
+function begin() {
   window.scrollTo(0, 0);
   main.style.WebkitAnimation = "fadeOut 0.5s";
   main.style.animation = "fadeOut 0.5s";
@@ -153,7 +160,7 @@ function begin(){
     setTimeout(() => {
       main.style.display = "none";
       qna.style.display = "block";
-    }, 0)
+    }, 0);
     let qIdx = 0;
     goNext(qIdx);
   }, 300);
